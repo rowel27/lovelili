2ee86112a546c412952cb9ea90b1b873a1f67fb5
rowel127 <rowel.espejo.044@my.csun.edu>
2025-08-26 15:39:56 -0700

    fix db1

--- backend/lovelili/settings.py
+++ backend/lovelili/settings.py
@@ -93,16 +93,16 @@ WSGI_APPLICATION = 'lovelili.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lovelili_db',
-        'USER': 'rowele',
-        'PASSWORD': 'MyPassword!',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+ #   'default': {
+     #   'ENGINE': 'django.db.backends.postgresql',
+     #   'NAME': 'lovelili_db',
+      #  'USER': 'rowele',
+     #   'PASSWORD': 'MyPassword!',
+     #   'HOST': 'localhost',
+     #   'PORT': '5432',
+   # }
+#}
 
 
 # Password validation
